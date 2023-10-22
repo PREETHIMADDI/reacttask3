1e24e5342ef39006b71c72c2e33a002ae1f11289
PREETHIMADDI <indumaddi9@gmail.com>
2023-10-22 20:26:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { ColorPicker } from './pages/colour';
+
+function App() {
+  return (
+    <div className="App">
+      <ColorPicker arr={['#fe0000','#00ff01','#0000fe','#ffff00','#ff00fe','#00ffff','#fea500','#81007f','#ffc0cb','#008001','#fe6347','#01ced1','#8b4512','#ff8b00','#4682b4','#fed700']}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/colour.js
@@ -0,0 +1,34 @@
+import { Component } from "react";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+export class ColorPicker extends Component{
+    constructor(props){
+        super(props);
+        this.state={pick:false,color:"white"};
+    }    
+    setColor=(index)=>{
+        this.setState({
+            color:this.props.arr[index],
+            pick:false
+        })
+    }
+    getColors=()=>{
+        return this.props.arr.map((color,index)=>{
+            return <button class="m-2" style={{backgroundColor:this.props.arr[index],width:40,height:40}} onClick={()=>this.setColor(index)} key={index}></button>
+        })
+    }
+    render(){
+        return(
+            <div className="p-5" >
+                <h2>Color Picker</h2>
+                <div className="shadow rounded-4 container col-9 d-flex align-items-center flex-column justify-content-center" style={{height:600,backgroundColor:this.state.color}}>
+                    <div class=''>
+                        {(this.state.pick)?this.getColors():null}
+                    </div>
+                    <button class="text-light" style={{backgroundColor:"#4caf50",width:'150px',height:'36px',fontSize:'18px'}} onClick={()=>this.setState({pick:true})}>Pick Color</button>
+                </div>
+            </div>
+        )
+    }
+}
+
